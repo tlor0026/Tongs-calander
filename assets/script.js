70b5833f13da9af141a46a3a70727ac6447e32e2
tlor0026 <tlor0026@gmail.com>
2022-06-01 22:27:23 -0500

    changes to time

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,9 @@ var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY")
 var currentHour = moment().format('h:mm:ss a');
 
 var interval = setInterval(function() {
-    var momentNow = moment();
-    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
-                        + momentNow.format('dddd')
+    var ct = moment();
+    $('#currentDay').html(ct.format('YYYY MMMM DD') + ' '
+                        + ct.format('dddd')
                          .substring(0,3).toUpperCase());
-    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+    $('#currentDay').html(currentDate + " " + ct.format('hh:mm:ss A'));
   }, 100);
-
-  
